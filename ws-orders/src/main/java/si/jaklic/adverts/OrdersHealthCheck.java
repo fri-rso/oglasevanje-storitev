5fd9cdbfd3d5595419509f174f12fb7ce0ec904d
Rok Jaklič <rok.jaklic@result.si>
2017-11-30 23:27:27 +0100

    .

--- ws-orders/src/main/java/si/jaklic/adverts/OrdersHealthCheck.java
+++ ws-orders/src/main/java/si/jaklic/adverts/OrdersHealthCheck.java
@@ -11,7 +11,7 @@ import java.util.logging.Logger;
 
 public class OrdersHealthCheck implements HealthCheck {
     @Inject
-    @DiscoverService(value = "my-service", environment = "dev", version = "1.0.0")
+    @DiscoverService(value = "orders-service", environment = "dev", version = "1.0.0")
     private String url;
 
     private static final Logger LOG = Logger.getLogger(OrdersHealthCheck.class.getSimpleName());
