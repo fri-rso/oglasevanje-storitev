25afe0fb737eee7165578135be628db977369088
Rok Jaklič <rok.jaklic@result.si>
2017-12-09 23:17:57 +0100

    .

--- ws-orders/src/main/java/si/jaklic/adverts/OrdersApplication.java
+++ ws-orders/src/main/java/si/jaklic/adverts/OrdersApplication.java
@@ -5,7 +5,8 @@ import com.kumuluz.ee.discovery.annotations.RegisterService;
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 
-@RegisterService(value = "orders-service", ttl = 20, environment = "dev", pingInterval = 15, version = "1.0.0", singleton = false)
+// @RegisterService(value = "orders-service", ttl = 20, environment = "dev", pingInterval = 15, version = "1.0.0", singleton = false)
+@RegisterService
 @ApplicationPath("/")
 public class OrdersApplication extends Application {
 }
