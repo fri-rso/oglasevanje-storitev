883d8c955e8b00236cf88423ff460b2424cdac63
Rok Jaklič <rok.jaklic@result.si>
2017-12-13 19:02:39 +0100

    .

--- ws-orders/src/main/java/si/jaklic/adverts/InfoResource.java
+++ ws-orders/src/main/java/si/jaklic/adverts/InfoResource.java
@@ -14,7 +14,7 @@ public class InfoResource {
     @GET
     public Response getOrders() {
         String[] clani = new String[]{"j2799", "lp7872", "lh7753"};
-        String[] mikrostoritve = new String[]{"http://35.198.158.135/orders", "http://35.198.120.63/adverts", "http://35.189.87.170/profiles"};
+        String[] mikrostoritve = new String[]{"http://35.205.24.229/orders", "http://35.205.77.9/adverts", "http://35.189.87.170/profiles"};
         String[] github = new String[]{"https://github.com/fri-rso/oglasevanje-storitev", "https://github.com/fri-rso/microservices-profile"};
         String[] travis = new String[]{"https://travis-ci.org/fri-rso/oglasevanje-storitev", "https://travis-ci.org/fri-rso/microservices-profile"};
         String[] dockerhub = new String[]{"https://hub.docker.com/r/frirso/ws-adverts/", "https://hub.docker.com/r/frirso/ws-orders/", "https://hub.docker.com/r/frirso/microservices-profile/"};
